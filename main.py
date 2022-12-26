253b66f31bf817fab2ac988e9281bbf7e3f3b12b
Nanantnu <121451274+Nanantnu@users.noreply.github.com>
2022-12-27 02:34:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
